8e9f0417eb85207d2be96d9dbd26162a023c27ce
KaminaSan <>
2016-11-12 21:34:39 +0900

    Added in Hash Adden in HashGenerator Changed UserDAO to use hashes

--- src/com/kam/SQL/DAO/UserDAO.java
+++ src/com/kam/SQL/DAO/UserDAO.java
@@ -7,8 +7,8 @@ package com.kam.SQL.DAO;
 
 import com.kam.DBUtil.DBUtil;
 import com.kam.SQL.ConnectionFactory;
-
 import com.kam.cms.beans.UserBean;
+import com.kam.cms.saltnhash.HashGenerator;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -45,25 +45,28 @@ public class UserDAO {
         return userList;
     }
 
-    public UserBean getSpecificUser(String user, String password) {
+    public UserBean getSpecificUser(String user, String password) { //cannot search by pass, we must retrieve pass and compare
         ResultSet rs = null;
         PreparedStatement ps = null;
         UserBean newBean = null;
         Connection con = getConnection();
        
         String getUserStatement = "SELECT * FROM users "
-                + "WHERE userName = ? "
-                + "AND userPass = ?";
+                + "WHERE userName = ?";
        
         try {
            
              ps = con.prepareStatement(getUserStatement);
             
             ps.setString(1, user);
-            ps.setString(2, password);
             rs = ps.executeQuery();
-            boolean userExists = rs.next();
-          
+            boolean userExists = rs.next();  //this tells us if we have any results
+           
+            if(userExists){
+                 String userHashedPass = rs.getString("userPass"); //get the hashedPass
+                 boolean userPassMatches = this.comparePassAndHash(password, userHashedPass); //compare the plain pass and see if it hashes into something similar to the DB
+                 userExists = userPassMatches;
+            }
             if (!userExists) {
                 System.out.println("User did not exist with username:"+ user+ "and pass:" + password);
                 return newBean;
@@ -74,7 +77,7 @@ public class UserDAO {
     String firstName = rs.getString("firstName");
     String lastName = rs.getString("lastName");
     String userEmail = rs.getString("userEmail");
-    String userPass = rs.getString("userPass");
+    String userPass = password; //if the hash matched, keep the plain text pass for future checks
     Integer userLevel = rs.getInt("userLevel");
     newBean = new UserBean(userId, userName, firstName, lastName, userEmail, userPass, userLevel);
      ps.close();
@@ -98,31 +101,70 @@ public class UserDAO {
     }
     
 
-        public String insertUser(UserBean newUser) {
-            String tempString = null;
-        return tempString;
+   public UserBean addUser(UserBean userToAdd){
+    Integer userId = null;
+    String userName = userToAdd.getUserName();
+     String firstName = userToAdd.getFirstName();
+   String lastName   = userToAdd.getLastName();
+    String userEmail = userToAdd.getUserEmail();
+   String userPass   = userToAdd.getUserPass();
+   String userHashedPass = this.getHashedPass(userPass); //we want to store the hashedPass
+    Integer userLevel = userToAdd.getUserLevel();
+     PreparedStatement stmt = null;
+     Connection con = null;
+     ResultSet rs = null;
+     UserBean newlyAddedUser = null;
+    String addUserSQL = "INSERT INTO users(userName, firstName, lastName, userEmail, userPass, userLevel)" 
+                        + " VALUES(?,?,?,?,?,?)";
+    
+    try{
+        con = this.getConnection();
+        stmt = con.prepareStatement(addUserSQL, Statement.RETURN_GENERATED_KEYS);
+        stmt.setString(1, userName);
+        stmt.setString(2, firstName);
+        stmt.setString(3, lastName);
+        stmt.setString(4, userEmail);
+        stmt.setString(5, userHashedPass); //store hashed pass only
+        stmt.setInt(6, userLevel);
+        
+        stmt.executeUpdate();
+        rs = stmt.getGeneratedKeys();
+        userId = rs.getInt(1);
+        newlyAddedUser = new UserBean(userId, userName, firstName, lastName, userEmail, userPass, userLevel);
     }
+    catch(SQLException ex){
+        ex.printStackTrace();
+    }
+    
+    finally{
+       DBUtil.closeStatement(stmt);
+       DBUtil.closeConnection(con);
+    }
+    return newlyAddedUser;
+   }
 
-        public String updateStudent(UserBean updatedStudent) {
-             String tempString = null;
-        return tempString;
-    }
+  
 
     
-        public boolean userInDatabase(String user, String password) {
+        public boolean userInDatabase(String user, String password) { //we have to work around looking for the user by password since the hash can change with JBCrypt
                 ResultSet rs = null;
         PreparedStatement ps = null;
         
         Connection con = getConnection();
-        String getUserStatement = "SELECT userName FROM users "
-                                + "WHERE userName = ? and userPass = ? ";
+        String getUserStatement = "SELECT userName, userPass FROM users "
+                                + "WHERE userName = ?";
 
         try {
              ps = con.prepareStatement(getUserStatement);
             ps.setString(1, user);
-            ps.setString(2, password);
+          
             rs = ps.executeQuery();
             boolean userExists = rs.next();
+            if(userExists){
+                String hashedPass = rs.getString("userPass");
+                boolean passwordMatch = comparePassAndHash(password, hashedPass);
+                userExists = passwordMatch;                                   //If the password matches the Hash, we have a true user match
+            }
             DBUtil.closeStatement(ps);
             return userExists;
 
@@ -143,7 +185,7 @@ public class UserDAO {
         PreparedStatement ps = null;
         boolean userInDatabase = false;
         
-        Connection con = ConnectionFactory.returnConnection("single");
+        Connection con = getConnection();
         String getUserStatement = "SELECT userName FROM users "
                                 + "WHERE userName = ?";
 
@@ -173,5 +215,18 @@ public class UserDAO {
         Connection con = ConnectionFactory.returnConnection("pooled");
         return con;
     }
-
+    
+    private String getHashedPass(String unHashedPass){
+        HashGenerator gen = HashGenerator.getInstance();
+        
+        return gen.getHash(unHashedPass);
+        
+     
+    }
+    
+    private boolean comparePassAndHash(String unHashedPass, String hashedPass){
+        HashGenerator gen = HashGenerator.getInstance();
+        return gen.testPassMatch(unHashedPass, hashedPass);
+        
+    }
 }
---
+++ src/com/kam/cms/saltnhash/HashGeneratorTester.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kam.cms.saltnhash;
+
+/**
+ *
+ * @author KaminaSan <www.kaminasan.com>
+ */
+public class HashGeneratorTester {
+    public static void main(String[] args) {
+       HashGenerator hashGen1 = HashGenerator.getInstance();
+       HashGenerator hashGen2 = HashGenerator.getInstance();
+       System.out.println("Hash Gen 1 and 2 are the same object:" + hashGen1.equals(hashGen2));
+       String firstPassword =  "xxx";
+       String secondPassword = "xxx";
+      
+       String firstHash = hashGen1.getHash(firstPassword);
+       String secondHash = hashGen2.getHash(secondPassword);
+       System.out.println("First Hash: " + firstHash + "     " + "Second Hash: " + secondHash);
+       System.out.println("First and Second Hash match? : " + firstHash.equals(secondHash));
+       
+       boolean firstPasswordMatchesHash = hashGen1.testPassMatch(firstPassword, secondHash);
+       boolean secondPasswordMatchesHash = hashGen2.testPassMatch(secondPassword, firstHash);
+       System.out.println("The First Password matches the first Hash: " + firstPasswordMatchesHash);
+       System.out.println("The Second Password matches the second Hash: " + secondPasswordMatchesHash );
+       System.out.println("Tester Finished");
+        
+        
+    }
+}
